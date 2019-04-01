5b06b8d7c2449f0bd005261f74f50566c8b096c6
artas182x <artas182x@gmail.com>
2019-04-01 20:53:09 +0200

    Disabled training

--- main.py
+++ main.py
@@ -18,14 +18,13 @@ def _parse_function(example_proto):
     return image, label
 
 
-IMG_SIZE = 160 # All images will be resized to 160x160
+IMG_SIZE = 96
 
-
-# Reizing imgages to 160x160 pixels
+# Reizing imgages
 def format_example(image, label):
     image = tf.cast(image, tf.float32)
     image = (image/127.5) - 1
-    image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+    #image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
 
     return image, label
 
@@ -33,6 +32,9 @@ def format_example(image, label):
 if __name__ == "__main__":
 
     keras = tf.keras
+
+   # keras.callbacks.TensorBoard(log_dir='./log', histogram_freq=0,
+    #                            write_graph=True, write_images=True)
 
     raw_dataset = tf.data.TFRecordDataset(["train.record"]).repeat()
     parsed_dataset = raw_dataset.map(_parse_function)
@@ -72,7 +74,7 @@ if __name__ == "__main__":
 
     feature_batch = base_model(image_batch)
     print(feature_batch.shape)
-    base_model.trainable = False
+    base_model.trainable = True
 
     global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
     feature_batch_average = global_average_layer(feature_batch)
@@ -98,8 +100,18 @@ if __name__ == "__main__":
         prediction_layer
     ])
 
+    # Let's take a look to see how many layers are in the base model
+    print("Number of layers in the base model: ", len(base_model.layers))
+
+    # Fine tune from this layer onwards
+    fine_tune_at = 100
+
+    # Freeze all the layers before the `fine_tune_at` layer
+    for layer in base_model.layers[:fine_tune_at]:
+        layer.trainable = False
+
     base_learning_rate = 0.0001
-    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate/10),
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
 
@@ -109,7 +121,7 @@ if __name__ == "__main__":
      #   for weight in SPLIT_WEIGHTS
     #)
 
-    initial_epochs = 3
+    initial_epochs = 4
     steps_per_epoch = int(train_size/BATCH_SIZE)
     validation_steps = int(val_size/BATCH_SIZE)
 
@@ -120,12 +132,25 @@ if __name__ == "__main__":
     cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                      save_weights_only=True,
                                                      verbose=1)
+
+    tb_callBack = keras.callbacks.TensorBoard(log_dir='./log', histogram_freq=1, write_graph=True, write_images=True,
+                                              update_freq=50)
+
+    #model = keras.models.load_model('hand_model.h5')
+
     # Training
-    history = model.fit(train_batches.repeat(),
-                        epochs=initial_epochs,
-                        steps_per_epoch=steps_per_epoch,
-                        validation_data=validation_batches.repeat(),
-                        validation_steps=validation_steps, callbacks=[cp_callback])
+    history = model.fit_generator(train_batches.repeat(),
+                                  epochs=initial_epochs,
+                                  workers=16, use_multiprocessing=False, max_queue_size=16,
+                                  verbose=1, steps_per_epoch=steps_per_epoch,
+                                  validation_data=validation_batches.repeat(),
+                                  validation_steps=validation_steps, callbacks=[cp_callback, tb_callBack])
+
+   # history = model.fit(train_batches.repeat(),
+    #                    epochs=initial_epochs,
+     #                   steps_per_epoch=steps_per_epoch,
+      #                  validation_data=validation_batches.repeat(),
+       #                 validation_steps=validation_steps, callbacks=[cp_callback, tb_callBack])
 
     model.save('hand_model.h5')
 
