Fixed image normalization and added probability threshold slider

camera_detection.py
@@ -4,6 +4,10 @@ import tensorflow as tf
 import cv2 as cv2
 import numpy as np
 
+alpha_slider_max = 100
+title_window = 'Camera hand detection example'
+prob_threshold = 0.5
+
 
 class FrameFragment:
     class_type = None
@@ -53,23 +57,37 @@ def split_image(image, __grid_size):
     return crop_list
 
 
+def on_trackbar(val):
+    global prob_threshold
+    prob_threshold = val / alpha_slider_max
+
 if __name__ == "__main__":
-    model = tf.keras.models.load_model('hand_model_gerard.h5')
+    model = tf.keras.models.load_model('hand_model.h5')
     grid_size = 96
     cap = cv2.VideoCapture(0)
     if cap.isOpened():
         height, width = get_proper_resolution(cap.get(4), cap.get(3), grid_size)
+        cv2.namedWindow(title_window)
         while True:
             ret, frame = cap.read()
             frame = cv2.resize(frame, (width, height))
+
+            brightness = 0
+            contrast = 50
+            img = np.int16(frame)
+            img = img * (contrast / 127 + 1) - contrast + brightness
+            img = np.clip(img, 0, 255)
+            frame = np.uint8(img)
+
             crops = split_image(frame, grid_size)
             images = []
             for crop in crops:
                 img = crop.image
-                #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
                 img = tf.cast(img, tf.float32)
-                img = (img / 127.5) - 1
+                img = (img / 255)
+               # img = tf.image.resize(img, (96, 96))
                 #  img = tf.reshape(img, [-1, grid_size, grid_size, 3])
                 images.append(img)
 
@@ -78,10 +96,13 @@ if __name__ == "__main__":
             print(images.shape)
             predictions = model.predict(images)
             for i in range(len(predictions)):
-                if predictions[i][0] > 0.8:
+                if predictions[i][0] >= prob_threshold:
                     cv2.rectangle(frame, (crops[i].x, crops[i].y),
                                   (crops[i].x + crops[i].width, crops[i].y + crops[i].height), (0, 255, 0), 1)
-            cv2.imshow('image', frame)
+
+            cv2.imshow(title_window, frame)
+            trackbar_name = 'Probability threshold x %d' % alpha_slider_max
+            cv2.createTrackbar(trackbar_name, title_window, 0, alpha_slider_max, on_trackbar)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         cap.release()

main.py
@@ -24,7 +24,7 @@ IMG_SIZE = 96
 # Reizing imgages
 def format_example(image, label):
     image = tf.cast(image, tf.float32)
-    image = (image / 127.5) - 1
+    image = (image / 255)
     # image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
 
     return image, label
@@ -32,6 +32,7 @@ def format_example(image, label):
 
 if __name__ == "__main__":
 
+    SHUFFLE_BUFFER_SIZE = 20000
     keras = tf.keras
     log_dir = os.path.join(".", "log")
     # keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0,
@@ -50,14 +51,13 @@ if __name__ == "__main__":
 
     raw_test = parsed_dataset_test
     raw_validation = parsed_dataset.take(val_size)
-    raw_train = parsed_dataset
+    raw_train = parsed_dataset.skip(val_size)
 
     train = raw_train.map(format_example)
     validation = raw_validation.map(format_example)
     test = raw_test.map(format_example)
 
-    BATCH_SIZE = 32
-    SHUFFLE_BUFFER_SIZE = 1000
+    BATCH_SIZE = 16
 
     train_batches = train.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
     validation_batches = validation.batch(BATCH_SIZE)
@@ -68,19 +68,22 @@ if __name__ == "__main__":
 
     IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 
-    # Own simple model - experimental - isn't used now
+    # Own simple model
     model = keras.models.Sequential()
     model.add(keras.layers.Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3),
                                   activation='relu'))
-    model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
     model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
     model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(256, activation='relu'))
-    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.Dense(1, activation='sigmoid'))
 
-    base_learning_rate = 0.0001
+    base_learning_rate = 0.0005
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
                   metrics=['accuracy'])